9604e5db0568de5fd66686467c312fab0f96de63
iriapirez <iriapirez@gmail.com>
2019-11-27 16:44:34 +0100

    hotfix

--- src/test/java/org/ctsqa/salesforce/actions/LeadsActions.java
+++ src/test/java/org/ctsqa/salesforce/actions/LeadsActions.java
@@ -11,7 +11,6 @@ import static org.ctsqa.salesforce.elements.LeadsElements.*;
 public class LeadsActions {
 
 
-
     public void leadsNew() {
 
         LEADS_DROPDOWN_BTN.click();
@@ -36,10 +35,11 @@ public class LeadsActions {
         SAVE_BTN.click();
     }
 
-    public void details(){
+    public void details() {
         DETAILS_BTN.click();
     }
-    public void moreActions(){
+
+    public void moreActions() {
         MORE_ACTIONS_BTN.click();
         CONVERT.click();
     }
@@ -52,40 +52,27 @@ public class LeadsActions {
         CONVERT_BTN.click();
         sleep(2000);
         CONVERT_BTN.click();
+    }
 
-        public void modifyOpportunity() {
-            GOTOLEADS_BTN.click();
-            OPPORTUNITY_ARROW.click();
-            sleep(1000);
-            FIRST_OPPORTUNITY.click();
-            DETAILS_LABEL.click();
-            EDIT_COMPETITORS.click();
-            COMPETITORS.setValue("some");
-            SAVE_OPPORTUNITY_BTN.click();
-        }
+    public void modifyOpportunity() {
+        GOTOLEADS_BTN.click();
+        OPPORTUNITY_ARROW.click();
+        sleep(1000);
+        FIRST_OPPORTUNITY.click();
+        DETAILS_LABEL.click();
+        EDIT_COMPETITORS.click();
+        COMPETITORS.setValue("some");
+        SAVE_OPPORTUNITY_BTN.click();
+    }
 
-        public void closeLead() {
-            CLOSED_TAB.scrollIntoView(false);
-            CLOSED_TAB.click();
-            SELECT_CLOSED.click();
-            sleep(2000);
-            //SELECT_CLOSED_DROPDOWN.click();
-            //SELECT_CLOSED_DROPDOWN.selectOptionByValue("_Closed_Placeholder_");
-            //SELECT_CLOSED_DROPDOWN.selectOptionByValue("Closed Won");
-            //SAVE_CLOSED_BTN.click();
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public void closeLead() {
+        CLOSED_TAB.scrollIntoView(false);
+        CLOSED_TAB.click();
+        SELECT_CLOSED.click();
+        sleep(2000);
+        //SELECT_CLOSED_DROPDOWN.click();
+        //SELECT_CLOSED_DROPDOWN.selectOptionByValue("_Closed_Placeholder_");
+        //SELECT_CLOSED_DROPDOWN.selectOptionByValue("Closed Won");
+        //SAVE_CLOSED_BTN.click();
     }
 }
